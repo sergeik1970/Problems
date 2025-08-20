a171e7895490ed1a50062a8e2dac5a15c8d725a3
Sergey Kudashev <sergey.m.kudashev@gmail.com>
2025-08-20 22:56:35 +0300

    Add solving Divide and conquer

--- Patterns/divide_and_conquer.js
+++ Patterns/divide_and_conquer.js
@@ -4,5 +4,33 @@
 
 // Примеры вызова функции:
 
-// binarySearch([1, 2, 3, 4, 5, 6], 4) // возвращает 3
-// binarySearch([1, 2, 3, 4, 5, 6], 11) // возвращает -1
+function binarySearchLazy(array, value) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function binarySearch(array, value) {
+    let left = 0;
+    let right = array.length - 1;
+    while (left <= right) {
+        let checker = Math.floor((right - left) / 2 + left)
+        
+        if (array[checker] === value) {
+            return checker;
+        }
+        else if (array[checker] < value) {
+            left = checker + 1;
+        }
+        else {
+            right = checker - 1;
+        }
+    }
+    return -1;
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 4)) // возвращает 3 
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 11)) // возвращает -1
